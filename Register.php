30e61777565e303baf91f9393e13144ef69f3f69
oscarhafeni <oscar.mbandeka@gmail.com>
2017-02-23 12:38:35 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("mysql10.000webhost.com", "a3288368_user", "abcd1234", "a3288368_data");
+    $con = mysqli_connect("mysql10.000webhost.com", "id893714_mbandeka", "january06", "id893714_oscar");
     
     $name = $_POST["name"];
     $age = $_POST["age"];
